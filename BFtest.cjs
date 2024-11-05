48c0af29be9762c0bc5877dba8a16585b806c909
neck392 <neck3922@gmail.com>
2024-11-06 00:29:28 +0900

    Update BFtest.cjs

--- BFtest.cjs
+++ BFtest.cjs
@@ -1,7 +1,5 @@
-// 조합에 사용할 문자 집합
 const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}|;:,.<>?';
 
-// 조합을 생성하는 함수
 function generateCombinations(length) {
     const combinations = [];
     const maxCombinations = Math.pow(chars.length, length);
@@ -15,18 +13,16 @@ function generateCombinations(length) {
             num = Math.floor(num / chars.length);
         }
 
-        console.log('생성된 조합:', combination); // 생성된 조합 출력
+        console.log('Generated combination:', combination);
         combinations.push(combination);
     }
 
     return combinations;
 }
 
-// 1부터 9까지의 길이로 조합 생성
 for (let length = 1; length <= 9; length++) {
-    console.log(`\n조합 길이: ${length}`);
+    console.log(`\nCombination length: ${length}`);
     const generatedCombinations = generateCombinations(length);
 
-    // 선택적으로 각 길이별 생성된 조합 수 출력
-    console.log(`총 생성된 조합 수 (길이 ${length}):`, generatedCombinations.length);
+    console.log(`Total combinations generated (length ${length}):`, generatedCombinations.length);
 }
